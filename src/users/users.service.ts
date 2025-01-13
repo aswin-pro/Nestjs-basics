9d57d1b6f4d2ae0cac28d383c92147fd5ff7fa8c
aswin-pro <aswinramesh2016@gmail.com>
2025-01-13 17:01:26 +0530

    dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @Injectable()
 export class UsersService {
@@ -6,7 +8,7 @@ export class UsersService {
         {
             "id" : 1,
             "name" : "Aswin",
-            "email" : "aswin@gmail.com",
+            "email": "aswin@gmail.com",
             "role" : "INTERN"
         },
         {
@@ -37,7 +39,9 @@ export class UsersService {
 
     findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
         if(role) {
-            return this.users.filter(user => user.role === role)
+            const rolesArray = this.users.filter(user => user.role === role)
+            if(rolesArray.length === 0) throw new NotFoundException('User Role Not Found')
+                return rolesArray
         }
         return this.users
     }
@@ -45,23 +49,25 @@ export class UsersService {
     findOne(id : number) {
         const user = this.users.find(user => user.id === id)
 
+        if (!user) throw new NotFoundException("User Not Found")
+
         return user
     }
 
-    create(user : { name : string, email:string, role:'INTERN' | 'ENGINEER' | 'ADMIN' }){
+    create(createUserDto : CreateUserDto){
        
         const newUser = {
             id: this.users.length+1,
-            ...user
+            ...createUserDto
         }
         this.users.push(newUser)
         return newUser
     }
 
-    update(id: number,updatedUser: { name? : string, email?:string, role?:'INTERN' | 'ENGINEER' | 'ADMIN' }){
+    update(id: number, updateUserDto : UpdateUserDto){
         this.users = this.users.map(user => {
             if(user.id === id){
-                return { ...user, ...updatedUser}
+                return { ...user, ...updateUserDto}
             }
             return user
         })
